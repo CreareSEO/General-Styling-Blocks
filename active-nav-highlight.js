6fa25f970782ef8cdec2e38d2b86ea43a3f2a22c
Curatron <jon.robinson@creare.co.uk>
2015-02-04 17:03:34 +0000

    Add Active Class to Navigation Links

--- active-nav-highlight.js
+++ active-nav-highlight.js
@@ -1,3 +1,6 @@
+//Change #easinav ul li a. to the path of your navigation 
+//Also the links in the navigation need to be exact match e.g www.creare.co.uk/contact not /contact 
+
 <script type="text/javascript">
 
 jQuery(document).ready(function() {
@@ -6,5 +9,3 @@ jQuery('#easinav ul li a[href="' + url + '"]').parent().addClass('active');
 });
 </script>
 
-<! - - - -  Change #easinav ul li a. to the path of your navigation - - - - - >
-<! - - - - Also the links in the navigation need to be exact match e.g www.creare.co.uk/contact not /contact - - - - >
