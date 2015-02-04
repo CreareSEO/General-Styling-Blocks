7e40f87b9fce7cbcd98c03f18006426081b22a56
Curatron <creare@1055M-JonRobinson.local>
2015-02-04 17:01:56 +0000

    Add Active Class to Nav Elements

---
+++ active-nav-highlight.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+jQuery(document).ready(function() {
+var url = window.location;
+jQuery('#easinav ul li a[href="' + url + '"]').parent().addClass('active');
+});
+</script>
+
+<! - - - -  Change #easinav ul li a. to the path of your navigation - - - - - >
+<! - - - - Also the links in the navigation need to be exact match e.g www.creare.co.uk/contact not /contact - - - - >
